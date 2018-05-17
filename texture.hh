37fb7a807cf1fb70957b45d83e658fddce4c48f5
Martin Nolte <nolte.mrtn@gmail.com>
2018-05-17 09:03:56 +0200

    [bugfix] stop flipping textures when saving to PNG files

--- texture.hh
+++ texture.hh
@@ -146,7 +146,7 @@ public:
     std::ofstream out( file );
     png::output png_out( out );
     png_out.write_info( width_, height_, 8, png::color_type_t::rgb_alpha );
-    png_out.write_image( pixels(), 4*width_, height_, true );
+    png_out.write_image( pixels(), 4*width_, height_ );
     png_out.write_end();
   }
 
